a30d4957fe3fd5e080205f0a2eefdf6901533cf5
Anel <anelt2010@gmail.com>
2015-08-24 20:22:38 -0500

    Parcial 2

---
+++ Parciales/parcial.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: 850 -*-
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
+	
+class ParcialApp(App):
+	pass
+
+if __name__ == '__main__':
+	ParcialApp().run()
